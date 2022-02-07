9cf77727c9b6c8c724ed7fa78e6a8ecb87ef8259
Moritz Waldleben <moritz.waldleben@gmail.com>
2022-02-07 16:29:55 +0100

    vanitygaps and more layouts

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 
 * configure colors through xresources
 * icons (Font Awesome 6) for tags
+* gaps between windows
 * some custom keybindings
 
 ## Patches
 
+* vanity gaps
 * xresources
--- config.def.h
+++ config.def.h
@@ -1,10 +1,15 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static unsigned int borderpx  = 3;        /* border pixel of windows */
-static unsigned int snap      = 32;       /* snap pixel */
-static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static char font[]            = "monospace:size=10";
 static char dmenufont[]       = "monospace:size=10";
 static const char *fonts[]          = { font };
@@ -17,7 +22,7 @@ static char selbgcolor[]            = "#005577";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor },
 };
 
 /* tagging */
@@ -39,11 +44,28 @@ static int nmaster     = 1;    /* number of clients in master area */
 static int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[D]",      deck },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ NULL,       NULL },
+
+	/* not used */
+	{ "TTT",      bstack },
+	{ "[@]",      dwindle },
+	{ "[@]",      spiral },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -93,11 +115,15 @@ static Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|Mod4Mask,              XK_g,      incrgaps,       {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_g,      incrgaps,       {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -106,14 +132,14 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,						XK_e,	   spawn,		   SHCMD("st -e neomutt") },
-	{ MODKEY,						XK_n,	   spawn,		   SHCMD("st -e ncmpcpp") },
-	{ MODKEY|ShiftMask,			    XK_w,	   spawn,		   SHCMD("passmenu") },
-	{ MODKEY|ShiftMask,			    XK_d,	   spawn,		   SHCMD("dunstctl close") },
-    { MODKEY|ShiftMask,				XK_p,	   spawn,		   SHCMD("mpc toggle") },
-    { MODKEY|ShiftMask,				XK_Left,   spawn,		   SHCMD("mpc prev") },
-	{ MODKEY|ShiftMask,				XK_Right,  spawn,		   SHCMD("mpc next") },
-	{ MODKEY|ShiftMask,				XK_r,	   spawn,		   SHCMD("mpc repeat") },
+	{ MODKEY,                       XK_e,      spawn,          SHCMD("st -e neomutt") },
+	{ MODKEY,                       XK_n,      spawn,          SHCMD("st -e ncmpcpp") },
+	{ MODKEY|ShiftMask,             XK_w,      spawn,          SHCMD("passmenu") },
+	{ MODKEY|ShiftMask,             XK_d,      spawn,          SHCMD("dunstctl close") },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          SHCMD("mpc toggle") },
+	{ MODKEY|ShiftMask,             XK_Left,   spawn,          SHCMD("mpc prev") },
+	{ MODKEY|ShiftMask,             XK_Right,  spawn,          SHCMD("mpc next") },
+	{ MODKEY|ShiftMask,             XK_r,      spawn,          SHCMD("mpc repeat") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
